94ab140db21b4685531dcd1d2cecd432af2888c1
ATuLa0 <ATuLa0@users.noreply.github.com>
2023-07-10 02:40:43 +0700

    Tạo mới Area riêng cho hệ thống quản lý

---
+++ CNPM_Quanlybanxedap/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CNPM_Quanlybanxedap.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "TrangChu", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
