a1dd7357e2e9b6ff2582a1becda0b764bc67b388
Lâm Nguyễn Thành <nguyenlamson200@gmail.com>
2023-08-12 15:19:16 +0700

    dưềw

--- CNPM_Quanlybanxedap/Models/Model1.Designer.cs
+++ CNPM_Quanlybanxedap/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Luong\Mon_Hoc\CNPM\CNPM_Quanlybanxedap\CNPM_Quanlybanxedap\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\DELL\OneDrive\Máy tính\CNPM_Quanlybanxedap\CNPM_Quanlybanxedap\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
